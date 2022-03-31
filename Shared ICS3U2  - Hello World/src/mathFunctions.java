74da06ec0d53b54c3c4dc935107f3647ff63923b
PINTOGavin <pintog001@tcdsb.ca>
2022-03-31 16:23:08 -0400

    added

--- Shared ICS3U2  - Hello World/src/mathFunctions.java
+++ Shared ICS3U2  - Hello World/src/mathFunctions.java
@@ -2,42 +2,42 @@ import java.util.Scanner;
 public class mathFunctions {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("1) Multiply two numbers");
 		System.out.println("2) Calculate a power of a number");
 		System.out.print("Choose an option: ");
 		int option = scanner.nextInt();
-		
+
 		switch(option) {
-		case 1:
-			System.out.print("Enter number 1: ");
-			int a = scanner.nextInt();
-			System.out.print("Enter number 2: ");
-			int b = scanner.nextInt();
-			System.out.println("The product is " + aTimesB(a,b));
-			break;
-		case 2:
-			System.out.print("Enter the base: ");
-			int base = scanner.nextInt();
-			System.out.print("Enter the exponent: ");
-			int exp = scanner.nextInt();
-			System.out.println("The answer is " + aToThePowerOfB(base,exp));
-			break;
-		case 3:
-			System.out.print("Enter the number to factorialize: ");
-			int n = scanner.nextInt();
-			System.out.println(n + " factorial is " + factorialOf(n));
-			break;
-		default:
-			System.out.println("Not a valid option.");
+			case 1:
+				System.out.print("Enter number 1: ");
+				int a = scanner.nextInt();
+				System.out.print("Enter number 2: ");
+				int b = scanner.nextInt();
+				System.out.println("The product is " + aTimesB(a,b));
+				break;
+			case 2:
+				System.out.print("Enter the base: ");
+				int base = scanner.nextInt();
+				System.out.print("Enter the exponent: ");
+				int exp = scanner.nextInt();
+				System.out.println("The answer is " + aToThePowerOfB(base,exp));
+				break;
+			case 3:
+				System.out.print("Enter the number to factorialize: ");
+				int n = scanner.nextInt();
+				System.out.println(n + " factorial is " + factorialOf(n));
+				break;
+			default:
+				System.out.println("Not a valid option.");
 		}
 	}
-	
+
 	public static int aTimesB(int a, int b) {
 		return a*b;
 	}
-	
+
 	public static int aToThePowerOfB(int a, int b) {
 		int power = 1;
 		if (a == 0) {
@@ -48,7 +48,7 @@ public class mathFunctions {
 		}
 		return power;
 	}
-	
+
 	public static int factorialOf(int a) {
 		for (int i = a-1; i > 0; i--) {
 			a *= i;
